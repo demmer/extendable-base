0d12056f9c954720a25bc6ab7665dc37b74edf25
Michael Demmer <demmer@jut.io>
2015-01-02 06:57:34 -0800

    update to grunt-dry 0.2.0

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,18 @@
 module.exports = function(grunt) {
     var pkg = grunt.file.readJSON('package.json');
     grunt.initConfig({
-        pkg: pkg
+        gruntDry: {
+            pkg: pkg,
+            deps: {
+                chai: {
+                    browserBuild: 'node_modules/chai/chai.js',
+                    testOnly: true
+                },
+                underscore: {
+                    browserBuild: 'node_modules/underscore/underscore.js'
+                }
+            }
+        }
     });
 
     grunt.task.loadNpmTasks('grunt-dry');
